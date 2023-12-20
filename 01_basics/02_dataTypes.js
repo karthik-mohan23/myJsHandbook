3c135c4bbbf87b03ba212a717525189da801fc4d
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-20 22:04:14 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+let name = "Karthik";
+let age = 18;
+let isLoggeedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => "" / ''
+// boolean => true/false
+// null => standalone value
+// undefined => haven't assigned value
+// symbol => unique
+
+// object
+
+console.log(typeof "car");
+console.log(typeof null); //object
