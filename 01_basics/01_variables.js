526083415200027a125571c1b9760efcedd3edb4
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-20 21:48:26 +0530

    started basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Kottayam";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
