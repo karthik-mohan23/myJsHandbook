e5cc62e1825221f357a7c9fd28e64e17f9d8df72
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-21 21:55:00 +0530

    numbers part 1

---
+++ 01_basics/07_nums.js
@@ -0,0 +1,8 @@
+const balance = new Number(100);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2)) => to fix decimal places to 2
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); => 10,00,000
+// console.log(hundreds.toLocaleString("en-IN")); => 10,00,000
