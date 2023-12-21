7a91bd4b5e57fcb8beb43a4820992c1c4c941ffb
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-21 14:46:35 +0530

    datatypes summary

---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,10 @@
+// Official docs => based how data is stored and retrieved(accessed) from memory
+// 1) Primitives & 2) Non-primitives
+
+// # Primitives(7)
+// ------------
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// # Non Primitives (Reference types)
+// ----------------
+// Arrays, Objects, Functions
