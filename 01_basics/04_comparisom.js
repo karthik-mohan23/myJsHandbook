96845c8f966ab904139b67c63b0e89ad7bb2df9e
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-21 11:33:49 +0530

    comparison of datatypes

---
+++ 01_basics/04_comparisom.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 !== 1);
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false => equality checks work differently
+// console.log(null >= 0); // true => comparisons convert null to a number(>,<,<=,>=)
+
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
+// console.log(undefined <= 0); //false
+
+// === => strict check, checks data types also
+// == => doesn't care about data types
